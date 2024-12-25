08a5816d25de8d9a25a93fb52b3c4f4555921c17
nc579009 <naomi.crowder@yahoo.com>
2024-12-25 14:44:33 -0500

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+// Player Clicks to attack the computer by clicking on the button
+// Player STATS
+// hull - 20
+// firepower - 5
+// accuracy - .7
+
+//If alieen ship is destroyed, player wins
+// If Alien shhip survives, it attack player
+// Computer StATS
+// hull - between 3and 6
+// firepower - between 2and 4
+// accuracy - between .6and .8
