0254c9ebb136abaa2d3b8ea3eb12d0fa4a3b3cac
nc579009 <naomi.crowder@yahoo.com>
2024-12-28 11:50:03 -0500

    Updates

--- app.js
+++ app.js
@@ -4,9 +4,17 @@
 // firepower - 5
 // accuracy - .7
 
-//If alieen ship is destroyed, player wins
-// If Alien shhip survives, it attack player
 // Computer StATS
 // hull - between 3and 6
 // firepower - between 2and 4
 // accuracy - between .6and .8
+
+// Create a check for hit accuracy
+// if hit, reduce computer ship hull by player firepower
+
+
+// ------ Game loop ------
+// //If alien ship is destroyed, player wins
+// If Alien shhip survives, it attack player
+// If player ship is destroyed, player loses
+//
